928c1d40c2ab7bffc0e0e06318a3d5d8176476c2
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-11-02 20:35:03 -0400

    completed naive lazy-load for revisions

--- lib/kron/domain/revision.rb
+++ lib/kron/domain/revision.rb
@@ -2,9 +2,17 @@ require_relative '../../../lib/kron/accessor/manifest_accessor'
 require_relative '../../../lib/kron/accessor/changeset_accessor'
 
 class Revision
+  include ChangesetAccessor
   include ManifestAccessor
-  attr_accessor :p_id, :id, :token, :changeset
-  # def manifest
-  #   manifest ||= load_manifest(id)
-  # end
+
+  attr_accessor :p_id, :id, :token
+  attr_writer :manifest, :changeset
+
+  def manifest
+    manifest ||= load_manifest(id) unless id.nil?
+  end
+
+  def changeset
+    changeset ||= load_changeset(id) unless id.nil?
+  end
 end
