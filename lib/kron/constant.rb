09a28e45c5b2266cd99f055b65903bdc3a933592
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-11-03 00:52:25 -0400

    move constants out of a class

--- lib/kron/constant.rb
+++ lib/kron/constant.rb
@@ -1,12 +1,8 @@
 require 'digest'
 
-class Constant
-  INDEX_FILE = 'index'.freeze
-  STAGE_FILE = 'stage'.freeze
-  REV_FILE = 'rev'.freeze
-  MANIFEST_DIR = 'manifest/'.freeze
-  CHANGESET_DIR = 'changeset/'.freeze
-  OBJECTS_BASE = 'objects/'.freeze
-
-  DIGEST_ALGO = Digest::SHA1
-end
+INDEX_FILE = 'index'.freeze
+STAGE_FILE = 'stage'.freeze
+REV_FILE = 'rev'.freeze
+MANIFEST_DIR = 'manifest/'.freeze
+CHANGESET_DIR = 'changeset/'.freeze
+OBJECTS_BASE = 'objects/'.freeze
