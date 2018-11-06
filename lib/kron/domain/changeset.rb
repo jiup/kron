52b24b9fa66734e489bd71a9d8a1ca64c72ad165
Tianqi Yang <tianqiyang@Tianqis-Mac.local>
2018-11-06 14:08:54 -0500

    add put and each to changeset

--- lib/kron/domain/changeset.rb
+++ lib/kron/domain/changeset.rb
@@ -2,20 +2,29 @@ module Kron
   module Domain
     class Changeset
       attr_accessor :commit_message, :timestamp, :account, :username, :added_files, :modified_files, :deleted_files
-    def initialize
-      @commit_message = "ttt"
+      attr_reader :revid
+    def initialize(revid)
+      @revid = revid
+      @commit_message = @account = @username = ""
+      @timestamp = Time.now.asctime
+      @added_files = @modified_files = @deleted_files = []
     end
-      def install(lines)
-        raise NotImplementedError
+
+      def put(param,value)
+        raise StandardError, "Cannot find this attribute in changeset!" unless instance_variable_get('@'+param)
+       instance_variable_set('@'+param,value)
       end
 
-      def attrs
-        instance_variables.map{|ivar| instance_variable_get ivar}
+      def each_attr(&blk)
+        instance_variables.each do |ivar|
+          value = instance_variable_get(ivar)
+          yield ivar, value
+        end
       end
     end
   end
 end
 
-a = Kron::Domain::Changeset.new
-att=a.instance_variables[0]
-p a.instance_variable_get(att)
+a = Kron::Domain::Changeset.new(1)
+a.put("commit_message", "time runs out")
+a.each_attr{|a,v| p a,v}
