1ee41acb8440f3a0e0420367bf4f62db7983f368
zhangyu <18301644230@163.com>
2018-11-07 16:30:46 -0500

    merge tianqi's index_accessor and stage_accessor

--- lib/kron/accessor/index_accessor.rb
+++ lib/kron/accessor/index_accessor.rb
@@ -1,12 +1,39 @@
+require_relative '../../kron/constant'
+require_relative '../domain/index'
+require 'zlib'
+require 'fileutils'
+
 module Kron
   module Accessor
     module IndexAccessor
-      def load_index
-        raise NotImplementedError
+
+      def init_dir(overwrite = false)
+        raise StandardError, 'directory \'index\' already exists' if !overwrite && Dir.exist?(INDEX_DIR)
+
+        FileUtils.mkdir_p INDEX_DIR
       end
 
-      def sync_index
-        raise NotImplementedError
+      def remove_dir
+        FileUtils.remove_dir INDEX_DIR, true
+      end
+
+      def load_index(revid)
+        idx = Kron::Domain::Index.new(revid)
+
+        src = File.join(INDEX_DIR, revid)
+        return nil unless File.file? src
+
+        Zlib::Inflate.inflate(File.read(src)).each_line do |line|
+          idx.put(line.chop.reverse.split(' ', 5).map(&:reverse).reverse)
+        end
+        idx
+      end
+
+      def sync_index(idx)
+        s_buf = StringIO.new
+        idx.each_pair { |path, attr| s_buf << "#{path} #{attr * ' '}\n" }
+        dst = File.join(INDEX_DIR, idx.revid)
+        File.open(dst, 'w+') { |f| f.write(Zlib::Deflate.deflate(s_buf.string)) }
       end
     end
   end
--- lib/kron/accessor/stage_accessor.rb
+++ lib/kron/accessor/stage_accessor.rb
@@ -1,12 +1,39 @@
+require_relative '../../kron/constant'
+require_relative '../domain/stage'
+require 'zlib'
+require 'fileutils'
+
 module Kron
   module Accessor
     module StageAccessor
-      def load_stage
-        raise NotImplementedError
+
+      def init_file(overwrite = false)
+        raise StandardError, 'stage already exists' if !overwrite && File.exist?(STAGE_PATH)
+        f = File.new(STAGE_PATH,"w")
+        f.close
       end
 
-      def sync_stage
-        raise NotImplementedError
+      def remove_file
+        File.delete(STAGE_PATH)
+      end
+
+      def load_stage
+        stg = Kron::Domain::Stage.new
+        Zlib::Inflate.inflate(File.read(STAGE_PATH)).each_line do |line|
+          params = line.chop.split(":",2)
+          stg.put(params[-1],params[0])
+        end
+        stg
+      end
+
+      def sync_stage(stg)
+        f = File.open(STAGE_PATH, "w")
+        line = ""
+        stg.each_stage do |path, head|
+          line += head + ":" + path + "\n"
+        end
+        f.syswrite(Zlib::Deflate.deflate(line))
+        f.close
       end
     end
   end
