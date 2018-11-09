1df42f225a025864e0ec21530591cf5097696a8e
Tianqi Yang <tianqiyang@Tianqis-Mac.local>
2018-11-08 20:12:38 -0500

    changeset updated

--- playground/test1/test.rb
+++ playground/test1/test.rb
@@ -6,8 +6,8 @@ require 'kron/repository'
 include Kron::Accessor::RevisionsAccessor
 include Kron::Repository
 
-add(['test1.txt'])
-commit('','Normal')
+# add(['test1.txt'])
+# commit('','Normal')
 # remove(["test.txt"])
 # FileUtils.copy "test.txt","test1.txt"
 # File.new(".kron/objects/as/test.txt","w") do |f|
